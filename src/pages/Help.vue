312b75af67f28c62842e382aa5f862c041e20fbc
Swaraj Upadhaya <swarajupadhaya781@gmail.com>
2021-01-06 20:03:59 +0530

    Todo list

---
+++ src/pages/Help.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help support</h5> 
+    <p> 
+      
+    </p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
